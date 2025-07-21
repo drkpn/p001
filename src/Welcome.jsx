fa1afc2d403d81a77fae3657ab8b7d83069b2ae9
drkpn <drkpn2019@gmail.com>
2025-07-21 14:30:18 +0530

    v1

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Welcome extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome Page</h1>
-                <a href='app'>App</a> <br/>
-                <a href='myapp'>MyApp</a>
+                <Link to="/app">Go to App</Link>
             </div>
         );
     }
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Myapp from './Myapp.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Welcome from './Welcome.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/p001">
+  <HashRouter>
     <Routes>
-      <Route path="app" element={<App/>}></Route>
-      <Route path='myapp' element={<Myapp/>}></Route>
+      <Route path="/app" element={<App/>}></Route>
+      <Route path='/myapp' element={<Myapp/>}></Route>
       <Route path='/' element={<Welcome/>}></Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
