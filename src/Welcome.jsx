d6b7197c43e33707e4a1db3fa54feede332e6ea0
drkpn <drkpn2019@gmail.com>
2025-07-21 15:27:38 +0530

    v1

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Welcome extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome Page</h1>
-                <Link to="/app">Go to App</Link>
+                <Link to="/app">App</Link> <br/>
                 <Link to="/myapp">MyApp</Link>
             </div>
         );
